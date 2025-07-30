ba868abab7a78411a4131274c75570131f7d4ddd
LngZhenFei1004 <longzhenfei1004@gmail.com>
2025-07-30 14:49:49 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,4 +3,33 @@ basic.forever(function () {
     led.setBrightness(255)
     basic.showIcon(IconNames.SmallHeart)
     led.setBrightness(100)
+    basic.showLeds(`
+        . # # # #
+        . # . . .
+        . # # # .
+        . # . . .
+        . # . . .
+        `)
+    led.setBrightness(255)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # #
+        . # . . .
+        . # # # .
+        . # . . .
+        . # # # #
+        `)
+    led.setBrightness(255)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    led.setBrightness(255)
+    basic.pause(500)
+    basic.showString("Fei")
+    basic.pause(2000)
 })
